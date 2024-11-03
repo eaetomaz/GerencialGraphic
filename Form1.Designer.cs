0dba7e1fb55b15ff3b2be3dcdad971ef4fa703b5
Guilherme Tomaz <158241415+eaetomaz@users.noreply.github.com>
2024-11-03 00:35:33 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace GerencialGraphic
+{
+    partial class FGraficoGerencial
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.wbGraficoGerencial = new System.Windows.Forms.WebBrowser();
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // wbGraficoGerencial
+            // 
+            this.wbGraficoGerencial.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.wbGraficoGerencial.Location = new System.Drawing.Point(0, 0);
+            this.wbGraficoGerencial.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wbGraficoGerencial.Name = "wbGraficoGerencial";
+            this.wbGraficoGerencial.Size = new System.Drawing.Size(1147, 561);
+            this.wbGraficoGerencial.TabIndex = 0;
+            // 
+            // webView21
+            // 
+            this.webView21.AllowExternalDrop = true;
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView21.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webView21.Location = new System.Drawing.Point(0, 0);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(1147, 561);
+            this.webView21.TabIndex = 1;
+            this.webView21.ZoomFactor = 1D;
+            // 
+            // FGraficoGerencial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1147, 561);
+            this.Controls.Add(this.webView21);
+            this.Controls.Add(this.wbGraficoGerencial);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FGraficoGerencial";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gráfico gerencial";
+            this.Load += new System.EventHandler(this.FGraficoGerencial_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser wbGraficoGerencial;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+    }
+}
+
